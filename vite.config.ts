318d5cf78d6fe80bb49c0df6a23f1de9dfefd2d1
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-06-01 11:33:14 +0545

    removing trackers

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import vitePluginInjectDataLocator from "./plugins/vite-plugin-inject-data-locator";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginInjectDataLocator()],
+  plugins: [react(), ],
 });
