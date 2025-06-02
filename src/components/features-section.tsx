13adaad376ee13ae03589cb76597e5ba560d5787
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-06-02 21:49:17 +0545

    made the scroll section responsive

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -2,35 +2,34 @@ import React from "react";
 import { ReactLenis } from 'lenis/react';
 import { Button, cn } from '@heroui/react';
 import { Icon } from "@iconify/react";
-import { motion } from "framer-motion";
 
 type allColors = 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'default'
 type featureType = { icon: string, title: string, description: string, color: allColors }
 
 type colorClasses = { icon?: string, main?: string, }
-const colors : Record<allColors, colorClasses> = {
+const colors: Record<allColors, colorClasses> = {
   primary: {
-    main: 'border-primary-200 bg-primary-50 p-2 text-primary-600',
-    icon : 'bg-gradient-to-br from-primary-400 to-primary-600'
+    main: 'border-primary-200 bg-primary-50 text-primary-600',
+    icon: 'bg-gradient-to-br from-primary-400 to-primary-600'
   },
   secondary: {
-    main: 'border-secondary-200 bg-secondary-50 p-2 text-secondary-600',
+    main: 'border-secondary-200 bg-secondary-50 text-secondary-600',
     icon: 'bg-gradient-to-br from-secondary-400 to-secondary-600'
   },
   success: {
-    main: 'border-success-200 bg-success-50 p-2 text-success-600',
+    main: 'border-success-200 bg-success-50 text-success-600',
     icon: 'bg-gradient-to-br from-success-400 to-success-600'
   },
   warning: {
-    main: 'border-warning-200 bg-warning-50 p-2 text-warning-600',
+    main: 'border-warning-200 bg-warning-50 text-warning-600',
     icon: 'bg-gradient-to-br from-warning-400 to-warning-600',
   },
   danger: {
-    main: 'border-danger-200 bg-danger-50 p-2 text-danger-600',
+    main: 'border-danger-200 bg-danger-50 text-danger-600',
     icon: 'bg-gradient-to-br from-danger-400 to-danger-600'
   },
   default: {
-    main: 'border-default-200 bg-default-50 p-2 text-default-600',
+    main: 'border-default-200 bg-default-50 text-default-600',
     icon: 'bg-gradient-to-br from-default-400 to-default-600'
   },
 }
@@ -68,29 +67,30 @@ const features: featureType[] = [
 export const FeaturesSection: React.FC = () => {
   return (
     <ReactLenis root>
-      <div className='flex justify-between px-2 md:px-16 '>
-        <div className='grid gap-2'>
-          {features.map(({icon , title, description, color}, i) => (<figure key={i} className='sticky top-0 h-screen grid place-content-center'>
-            <article className={cn(
-              'h-72 w-[30rem] p-4 rounded-lg grid place-content-center gap-4',
+      <div className='flex flex-col-reverse md:flex-row justify-between  gap-8 px-2 md:px-16'>
+        <div>
+          {features.map(({ icon, title, description, color }, i) => (
+            <div key={i} className='sticky pt-[40vh] md:pt-0 top-0 h-[100vh] md:h-screen place-content-center'>
+            <div className={cn(
+              'h-72 w-[30rem] max-w-[80%] p-4 mx-auto rounded-lg grid place-content-center gap-4',
               colors[color].main,
               i % 3 === 1 && 'rotate-6',
               i % 3 === 2 && '-rotate-6'
             )}>
-              <div className={ cn("flex h-12 w-12 items-center justify-center rounded-lg p-3 text-white", colors[color].icon) }>
+              <div className={cn("flex h-12 w-12 items-center justify-center rounded-lg p-3 text-white", colors[color].icon)}>
                 <Icon icon={icon} className="text-2xl" />
               </div>
-              <h1 className='text-2xl font-semibold'>{title}</h1>
-              <p>{description}</p>
-              <Button color ={color}>Learn More</Button>
-            </article>
-          </figure>))}
+              <div className='text-2xl font-semibold'>{title}</div>
+              <div>{description}</div>
+              <Button color={color}>Learn More</Button>
+            </div>
+          </div>))}
         </div>
-        <div className='sticky top-0 h-screen grid place-content-center'>
+        <div className='sticky top-0 h-screen pt-20 -pb-20 md:pt-0 flex-col flex items-center md:items-end text-center md:text-right md:place-content-center'>
           <h2 className="mb-4 text-3xl font-bold md:text-4xl">
             Powerful <span className="gradient-text">Features</span> to Boost Your Productivity
           </h2>
-          <p className="mx-auto max-w-2xl text-foreground-600">
+          <p className="text-foreground-600 max-w-lg">
             Designed to help you focus, organize, and accomplish more in less time with less stress.
           </p>
         </div>
